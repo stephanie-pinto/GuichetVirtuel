b22c4c58a50d032ab2b37c67da9f4a4c616b5189
Stéphanie Pinto <stephanie.pinto1@students.hevs.ch>
2018-06-08 14:50:58 +0200

    Ajoutez des fichiers projet.

---
+++ RegistreFoncier/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RegistreFoncier.Startup))]
+namespace RegistreFoncier
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
