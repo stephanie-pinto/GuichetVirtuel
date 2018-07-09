a98f5de3d76c9785848d58ab82d40c59b3400a5c
Stéphanie Pinto <stephanie.pinto1@students.hevs.ch>
2018-07-09 19:26:15 +0200

    Base

--- RegistreFoncier/Controllers/HomeController.cs
+++ RegistreFoncier/Controllers/HomeController.cs
@@ -11,7 +11,43 @@ namespace RegistreFoncier.Controllers
         public ActionResult Index()
         {
             ViewBag.Title="Guichet virtuel";
-return View();
+            return View();
+        }
+
+        public ActionResult Mission()
+        {
+            ViewBag.Title = "Notre mission";
+            return View();
+        }
+
+        public ActionResult Centrale()
+        {
+            ViewBag.Title = "Centrale des testaments";
+            return View();
+        }
+
+        public ActionResult RF2020()
+        {
+            ViewBag.Title = "Registre foncier 2020";
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Title = "Contactez-nous";
+            return View();
+        }
+
+        public ActionResult RF()
+        {
+            ViewBag.Title = "Registre foncier";
+            return View();
+        }
+
+        public ActionResult FAQ()
+        {
+            ViewBag.Title = "Foire aux questions";
+            return View();
         }
     }
 }
